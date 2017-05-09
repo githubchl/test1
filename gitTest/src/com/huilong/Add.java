37d234168835e12323b8aa9b6aefebb532a255f0
githubchl <2416709025@qq.com>
2017-05-09 22:39:31 +0800

    第二次提交，这是java代码

---
+++ gitTest/src/com/huilong/Add.java
@@ -0,0 +1,8 @@
+package com.huilong;
+
+public class Add {
+	public Add(){
+		System.out.println("add功能未实现");
+	}
+
+}
---
+++ gitTest/src/com/huilong/Del.java
@@ -0,0 +1,7 @@
+package com.huilong;
+
+public class Del {
+	public Del(){
+		System.out.println("del功能未实现");
+	}
+}
